d6a764fcefe8ca6170bf9e5614cf44a47e7846f9
T-dubb <rlaxodntttt@kaist.ac.kr>
2024-09-20 16:49:27 +0900

    feat: firebase 연결

---
+++ src/firebaseConfig.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSw153L7JKyvNudgdYpZVUiA_MRRGF9_s",
+  authDomain: "again-server-5156a.firebaseapp.com",
+  projectId: "again-server-5156a",
+  storageBucket: "again-server-5156a.appspot.com",
+  messagingSenderId: "476144887234",
+  appId: "1:476144887234:web:84452d61eb6b2391cda25b",
+  measurementId: "G-KZE6H374G2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app)
+
+export { db };
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import firebaseApp from './firebaseConfig';  // Firebase 설정 가져오기
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
