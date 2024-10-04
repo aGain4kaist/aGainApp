85f180ec4d1a2afded85db0c66648acfc6e83f02
T-dubb <rlaxodntttt@kaist.ac.kr>
2024-10-04 13:43:21 +0900

    feat: .env setting

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -8,14 +8,15 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 환경 변수에서 Firebase 설정 불러오기
 const firebaseConfig = {
-  apiKey: 'AIzaSyDSw153L7JKyvNudgdYpZVUiA_MRRGF9_s',
-  authDomain: 'again-server-5156a.firebaseapp.com',
-  projectId: 'again-server-5156a',
-  storageBucket: 'again-server-5156a.appspot.com',
-  messagingSenderId: '476144887234',
-  appId: '1:476144887234:web:84452d61eb6b2391cda25b',
-  measurementId: 'G-KZE6H374G2',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
