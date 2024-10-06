4f5f9eb3c9427893a986297f04237e503b829d2f
T-dubb <rlaxodntttt@kaist.ac.kr>
2024-10-06 18:26:17 +0900

    fix: header, body, nav layout

--- src/pages/PartySearch.jsx
+++ src/pages/PartySearch.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { useUser } from '../utils/UserContext';
 import KakaoMap from '../utils/KakaoMap';
+import { Box, Flex } from '@chakra-ui/react';
 
 function PartySearch() {
   const { user, setUser } = useUser();
   return (
-    <>
-      <div>
-        PartySearch
-        <div>
-          <h1>Welcome, {user ? user.name : 'Guest'}!</h1>
-          <button onClick={() => setUser({ name: 'John Doe' })}>
-            Set User
-          </button>
-        </div>
-      </div>
-      <div id="map" style={{ width: '100%', height: '50vh' }}>
+    <Flex direction="column" height="100vh">
+      {/* 헤더 */}
+      <Box bg="gray.100" padding="20px" textAlign="center">
+        <h1>Welcome, {user ? user.name : 'Guest'}!</h1>
+        <button onClick={() => setUser({ name: 'John Doe' })}>Set User</button>
+      </Box>
+
+      {/* 카카오맵이 가운데 공간을 채움 */}
+      <Box flex="1" width="100%">
+        {/* 남은 공간을 채움 */}
         <KakaoMap />
-      </div>
-    </>
+      </Box>
+    </Flex>
   );
 }
 
