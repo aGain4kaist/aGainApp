d3087e97c6fd14772d27e44afd7adae2bd483a75
T-dubb <rlaxodntttt@kaist.ac.kr>
2024-10-28 01:01:02 +0900

    fix: 지도 안불러와지는 에러 해결

--- src/utils/KakaoMap.jsx
+++ src/utils/KakaoMap.jsx
@@ -1,13 +1,13 @@
-// KakaoMap.jsx
 import React from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 function KakaoMap({ partyListData, handlePartyClick }) {
+  console.log('hello');
   return (
     <Map
-      center={{ lat: 36.370379109284, lng: 127.36265917051 }} // 지도 중심 설정
+      center={{ lat: 36.370379109284, lng: 127.36265917051 }}
       style={{ width: '100%', height: '100%' }}
-      level={4} // 확대 수준
+      level={4}
     >
       {partyListData.map((party) => (
         <MapMarker
@@ -15,10 +15,10 @@ function KakaoMap({ partyListData, handlePartyClick }) {
           position={{ lat: party.location[0], lng: party.location[1] }}
           image={{
             src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
-            size: { width: 24, height: 35 }, // 마커 이미지 크기
+            size: { width: 24, height: 35 },
           }}
           title={party.name}
-          onClick={() => handlePartyClick(party)} // 마커 클릭 시 파티 정보 전달
+          onClick={() => handlePartyClick(party)}
         />
       ))}
     </Map>
