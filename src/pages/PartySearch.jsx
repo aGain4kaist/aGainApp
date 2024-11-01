324074567aa0105916670b5c085a943a808e18cd
Ryu Ihchae <ihchaeryu@kaist.ac.kr>
2024-11-01 13:57:46 +0900

    trivial commit

--- src/pages/PartySearch.jsx
+++ src/pages/PartySearch.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { useUser } from '../utils/UserContext';
 import KakaoMap from '../utils/KakaoMap';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import Header from '../components/Layout/Header';
 import PartyListBottomSheet from '../components/PartyListBottomSheet';
 import { partyListData } from '../data/partyListData';
+import { BottomSheet } from 'react-spring-bottom-sheet'
+import 'react-spring-bottom-sheet/dist/style.css'
 
 function PartySearch() {
   const { user } = useUser();
@@ -15,6 +17,8 @@ function PartySearch() {
     lng: 127.36265917051,
   });
   const [myLocation, setMyLocation] = useState(null);
+  // changing bottom sheet...
+  const [open, setOpen] = useState(true)
 
   const handlePartyClick = (party) => {
     setSelectedParty(party);
@@ -49,6 +53,23 @@ function PartySearch() {
 
   return (
     <Flex direction="column" height="100vh" position="relative">
+      <BottomSheet 
+        open={open}
+        blocking={false}
+        header={
+          <Flex justifyContent="space-between" alignItems="center" my={4} px={2}>
+            <Text fontSize="2xl" fontWeight="bold">
+              내게 가까운 파티들
+            </Text>
+            <Text fontSize="md" color="purple.500">
+              거리순 ▼
+            </Text>
+          </Flex>
+        }
+        snapPoints={({ minHeight, maxHeight }) => [maxHeight / 3, maxHeight * 0.9]}
+      >
+        My awesome content here
+      </BottomSheet>
       {!isExpanded && (
         <Header
           user={user}
@@ -72,8 +93,8 @@ function PartySearch() {
           myLocation={myLocation}
         />
       </Box>
-      <Box position="relative" zIndex="2">
-        <PartyListBottomSheet
+      {/*<Box position="relative" zIndex="2">
+         <PartyListBottomSheet
           isExpanded={isExpanded}
           setIsExpanded={setIsExpanded}
           partyListData={partyListData}
@@ -84,8 +105,8 @@ function PartySearch() {
             setIsExpanded(false);
           }}
           goToCurrentLocation={goToCurrentLocation} // 함수를 전달
-        />
-      </Box>
+        /> 
+      </Box>*/}
     </Flex>
   );
 }
