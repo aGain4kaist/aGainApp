cc2765c65f3e334dac9a7916e0721cac6fc5ee5f
abrastone <127582917+abrastone@users.noreply.github.com>
2024-11-02 09:46:31 +0900

    feat: 파티 세부페이지 UI 구현

--- src/components/PartyDetail.jsx
+++ src/components/PartyDetail.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Box, Flex, Text, Button, Image } from '@chakra-ui/react';
+import { Icon as IconifyIcon } from '@iconify/react';
 
 function PartyDetail({ party, onBack }) {
   // 파티 시작 및 종료 시간 포맷
@@ -9,11 +10,16 @@ function PartyDetail({ party, onBack }) {
   const imageUrl = party.image
     ? `/src/assets/images/partyImages/${party.image[0]}`
     : null;
+  const [isFavorite, setIsFavorite] = useState(false); // 파티의 좋아요 상태 가져와야됨
+  const handleFavClick = (e) => {
+    e.stopPropagation(); // Prevent triggering the onPartyClick
+    setIsFavorite(!isFavorite);
+  };
   return (
-    <Box px={4} pt={4} position="relative">
+    <Box px="35px" pt="52px" position="relative" zIndex={20}>
       {/* 파티 이미지 또는 기본 박스 */}
       <Box
-        width="90%"
+        width="100%"
         height="200px"
         bg="gray.200"
         mx="auto"
@@ -40,24 +46,103 @@ function PartyDetail({ party, onBack }) {
       </Box>
 
       {/* 파티 정보 */}
-      <Box mb={4}>
-        <Text fontSize="sm" color="gray.500" mb="1">
+      <Box>
+        <Text color="#411461" fontWeight="semibold">
           내 위치로부터 ・ {party.distance}km
         </Text>
-        <Text fontSize="2xl" fontWeight="bold" mb="2">
+        <Text fontSize="32px" fontWeight={800}>
           {party.name}
         </Text>
-        <Text mb="2">⭐ {party.favs}</Text>
-        <Text mb="1">{`${startTime.getMonth() + 1}월 ${startTime.getDate()}일 (${['일', '월', '화', '수', '목', '금', '토'][startTime.getDay()]})`}</Text>
-        <Text mb="1">{formatTime(startTime)}</Text>
-        <Text mb="2">{party.address}</Text>
-        <Text color="gray.500">{party.host[1]}님이 호스트</Text>
+        <Flex alignItems="center">
+          <IconifyIcon
+            icon={
+              isFavorite ? 'ant-design:star-filled' : 'ant-design:star-outlined'
+            }
+            style={{ color: 'black', fontSize: '25px', marginRight: '10px' }}
+            onClick={handleFavClick}
+          />
+          <Text fontSize="lg" fontWeight={500}>
+            {party.favs}
+          </Text>
+        </Flex>
+        <Box height="15px"></Box>
+
+        <Flex direction="column" gap="5px">
+          <Flex alignItems="center">
+            <IconifyIcon
+              icon="uis:calender"
+              style={{ fontSize: '18px', marginRight: '10px' }}
+            />
+            <Text color="#7D7D7D" fontWeight={500}>
+              {`${startTime.getMonth() + 1}월 ${startTime.getDate()}일 (${['일', '월', '화', '수', '목', '금', '토'][startTime.getDay()]})`}
+            </Text>
+          </Flex>
+          <Flex alignItems="center">
+            <IconifyIcon
+              icon="mdi:clock"
+              style={{ fontSize: '18px', marginRight: '10px' }}
+            />
+            <Text color="#7D7D7D" fontWeight={500}>
+              {formatTime(startTime)}
+            </Text>
+          </Flex>
+          <Flex alignItems="center">
+            <IconifyIcon
+              icon="ic:round-location-on"
+              style={{ fontSize: '18px', marginRight: '10px' }}
+            />
+            <Text color="#7D7D7D" fontWeight={500}>
+              {party.address}
+            </Text>
+          </Flex>
+          <Flex alignItems="center">
+            <IconifyIcon
+              icon="jam:crown-f"
+              style={{ fontSize: '18px', marginRight: '10px' }}
+            />
+            <Text color="#7D7D7D" fontWeight={500}>
+              {party.host[1]}님이 호스트
+            </Text>
+          </Flex>
+        </Flex>
       </Box>
 
+      <Box height="71px"></Box>
+
       {/* 버튼 */}
-      <Flex direction="column" gap={2} mt="4">
-        <Button colorScheme="purple">내 옷 등록하기</Button>
-        <Button colorScheme="purple" variant="outline">
+      <Flex
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="12px"
+      >
+        <Button
+          width="200px"
+          height="51px"
+          gap="10px"
+          borderRadius="25px"
+          backgroundColor="#7C31B4"
+          boxShadow="0px 2px 4px 2px rgba(0, 0, 0, 0.25)"
+          _hover={{ backdropFilter: 'blur(25px)' }}
+          color="white"
+          fontSize="lg"
+          fontWeight={700}
+        >
+          내 옷 등록하기
+        </Button>
+        <Button
+          width="200px"
+          height="51px"
+          gap="10px"
+          borderRadius="25px"
+          backgroundColor="#E8E8E8"
+          boxShadow="0px 2px 4px 2px rgba(0, 0, 0, 0.25)"
+          _hover={{ backdropFilter: 'blur(25px)' }}
+          color="black"
+          fontSize="lg"
+          fontWeight={700}
+        >
           등록된 옷 확인하기
         </Button>
       </Flex>
