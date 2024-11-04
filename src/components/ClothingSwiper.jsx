d631c6bbd9b7a8285da3f556e72030ae05b7b553
minb__m <kmb3322@naver.com>
2024-11-05 01:52:37 +0900

    feat: Home화면 ClothingSwiper 11월 4일자 수정본입니다.

--- src/components/ClothingSwiper.jsx
+++ src/components/ClothingSwiper.jsx
@@ -6,12 +6,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 function ClothingSwiper({ items }) {
   return (
     <Box mt={6}>
-      <Text fontSize="lg" fontWeight="bold" mb={2}>
-        주변에 이런 옷이 등록됐어!
+      <Text fontSize="2xl" fontWeight="bold" mb={2}>
+        새로 등록된 옷이에요
       </Text>
       <Swiper spaceBetween={20} slidesPerView="auto">
         {items.map((item) => (
           <SwiperSlide key={item.id} style={{ width: '150px' }}>
+            <Box mt={1}></Box>
             <Box
               bg="white"
               borderRadius="20px"
@@ -28,18 +29,43 @@ function ClothingSwiper({ items }) {
                 height="150px"
                 borderRadius="20px 20px 0px 0px"
               />
-                <Box mt={1}></Box>
-              <Text fontSize="md" fontWeight="bold" color="purple.700" mb={-1.5}>
+              <Box mt={1.5}></Box>
+              <Text
+                style={{
+                  color: 'var(--21-purple-dark, #411461)',
+                  textAlign: 'center',
+                  fontFamily: 'SUIT',
+                  fontSize: '16px',
+                  fontStyle: 'normal',
+                  fontWeight: 600,
+                  lineHeight: 'normal',
+                  marginBottom: '-4px'
+                }}
+              >
                 {item.name}
               </Text>
-              <Text fontSize="sm" fontWeight="light" color="gray.600">
-                {' '}
-                {item.brand}
-              </Text>
-              <Text fontSize="xs" fontWeight="bold" color="gray.600" mb={2}>
-                {item.size}
-              </Text>
+
+              <Box mt={1}></Box>
+
+              <Text
+                style={{
+                  color: 'var(--subtitle-Gray, #7D7D7D)',
+                  textAlign: 'center',
+                  fontFamily: 'SUIT',
+                  fontSize: '10px',
+                  fontStyle: 'normal',
+                  fontWeight: 500,
+                  lineHeight: 'normal',
+                  marginBottom: '8px'
+                }}
+              >
+                {item.foot ? `신발 사이즈 ${item.foot}` : (item.height && item.weight ? `${item.height} ${item.weight} · 사이즈 ${item.size}` : '사이즈 비공개')}
+                </Text>
+                {/* 'weight, height' 데이터가 있을시 "키 몸무게 ˑ 사이즈 __함", 'foot' 데이터가 있을시 "신발 사이즈 __mm", 아무것도 없을시 '사이즈 비공개 */}
+
             </Box>
+            <Box mt={1}></Box>
+
           </SwiperSlide>
         ))}
       </Swiper>
