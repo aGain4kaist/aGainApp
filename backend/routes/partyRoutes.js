f4c208f93cede1830fa80875c70fc451b93afb3e
not-kim16 <kimjinhwi0416@gmail.com>
2024-11-07 16:18:04 +0900

    backend/routes/partyRoutes.js 수정

--- backend/routes/partyRoutes.js
+++ backend/routes/partyRoutes.js
@@ -8,6 +8,7 @@ const {
 
 router.get('/', getAllParties);
 router.get('/:id', getPartyById);
-router.get('/clothes/:id', getClothesOfParty);
+router.get('/:id/clothes', getClothesOfParty); // 파티에 속한 모든 옷을 불러온다.
+router.get('/:id/users', getUsersOfParty); // 파티에 속한 모든 유저를 불러온다.
 
 module.exports = router;
