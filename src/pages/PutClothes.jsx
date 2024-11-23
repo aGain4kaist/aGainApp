bfa2a4cafc6e5bac3e614cc97693afadbcb13392
minb__m <kmb3322@naver.com>
2024-11-24 03:28:37 +0900

    input width 조정

--- src/pages/PutClothes.jsx
+++ src/pages/PutClothes.jsx
@@ -9,7 +9,7 @@ import {
   TagCloseButton,
   TagLabel,
   Text,
-  Textarea,
+  Textarea
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -17,16 +17,16 @@ import { useNavigate } from 'react-router-dom';
 // 커스텀 BackIcon
 const BackIcon = (props) => (
   <Icon
-    width="11px" // 아이콘의 너비 설정
-    height="20px" // 아이콘의 높이 설정
+    width="11px"
+    height="20px"
     viewBox="0 0 11 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    {...props} // 추가적인 props 전달 가능
+    {...props}
   >
     <path
       d="M10 19L1 10L10 1"
-      stroke="currentColor" // 현재 텍스트 색상과 동일하게 설정
+      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -36,12 +36,12 @@ const BackIcon = (props) => (
 
 function PutClothes() {
   const navigate = useNavigate();
-  const [story, setStory] = useState(''); // "옷 이야기" 입력
-  const [keywords, setKeywords] = useState([]); // "옷 키워드 태그" 입력
-  const [nickname, setNickname] = useState(''); // "옷 별명" 입력
+  const [story, setStory] = useState('');
+  const [keywords, setKeywords] = useState([]);
+  const [nickname, setNickname] = useState('');
 
-  const [height, setHeight] = useState(''); // 키 상태 변수
-  const [weight, setWeight] = useState(''); // 몸무게 상태 변수
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
 
   //키워드 (추후 AI 사용) 더미데이터
   const dummyKeywords = ['따뜻한', '부드러운', '베이지 색', '니트', '남성복'];
@@ -69,20 +69,20 @@ function PutClothes() {
     <Flex
       direction="column"
       align="center"
-      bg="var(--background-silver, #FAF9FF)" // 배경색 업데이트
+      bg="var(--background-silver, #FAF9FF)"
       minHeight="100vh"
       width="100%"
     >
       {/* 상단 헤더 */}
       <Flex align="center" width="100%" mb="55px" mt="50px" ml="30px">
         <Button
-          onClick={() => navigate(-1)} // 뒤로가기 버튼 동작
+          onClick={() => navigate(-1)}
           variant="ghost"
           color="#000000"
           left="0"
           padding="0"
         >
-          <BackIcon /> {/* 커스텀 BackIcon 적용 */}
+          <BackIcon />
         </Button>
 
         <Text textAlign="center" fontSize="24px" fontWeight="700">
@@ -104,7 +104,7 @@ function PutClothes() {
         style={{ backdropFilter: 'blur(25px)' }}
       >
         <Image
-          src="/camera.png" // public 폴더의 camera.png 경로
+          src="/camera.png"
           alt="Camera Icon"
           boxSize="60px"
           objectFit="cover"
@@ -119,23 +119,24 @@ function PutClothes() {
         width="100%"
         maxWidth="600px"
         overflowY="auto"
-        paddingBottom="20px" // 스크롤 시 하단 여백 확보
+        paddingBottom="20px"
       >
         {/* 옷 별명 */}
         <Flex
           mb="50px"
           flexDirection={{ base: 'column', sm: 'row' }}
-          alignItems="flex-start"
+          alignItems="center"
+          width="100%"
+          minHeight="72px" // 최소 높이 설정
         >
           <Text
             fontFamily="SUIT"
             fontSize="20px"
-            fontStyle="normal"
             fontWeight="700"
-            lineHeight="normal"
             color="var(--21-purple-dark, #411461)"
-            mr={{ base: '0', sm: '53px' }}
             mb={{ base: '10px', sm: '0' }}
+            mr={{ base: '0', sm: '30px' }} // 'sm' 이상에서 30px 간격
+            width={{ base: '100%', sm: 'auto' }}
           >
             옷 별명
           </Text>
@@ -144,24 +145,22 @@ function PutClothes() {
             borderRadius="12px"
             bg="#FFFFFF"
             fontSize="14px"
-            width={{ base: '100%', sm: '252px' }}
             height="32px"
-            flexShrink={0}
+            width={{ base: '100%', sm: 'auto' }}
+            flex="1"
             boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
             backdropFilter="blur(25px)"
-            ml={{ base: '0', sm: 'auto' }}
             value={nickname}
             onChange={(e) => setNickname(e.target.value)}
           />
         </Flex>
 
+
         {/* 내 옷의 이야기 */}
         <Text
           fontFamily="SUIT"
           fontSize="20px"
-          fontStyle="normal"
           fontWeight="700"
-          lineHeight="normal"
           color="var(--21-purple-dark, #411461)"
           mb="10px"
         >
@@ -215,19 +214,19 @@ function PutClothes() {
                 borderRadius="15px"
                 variant="solid"
                 colorScheme="purple"
-                bg="var(--lightlight-Gray, #f0f0f0)" // 배경색 적용
+                bg="var(--lightlight-Gray, #f0f0f0)"
                 display="flex"
                 padding="5px 8px"
                 alignItems="center"
                 gap="10px"
                 color="#000000"
-                cursor="pointer" // 클릭 가능 표시
-                onClick={() => handleTagClick(keyword)} // 태그 클릭 핸들러 추가
+                cursor="pointer"
+                onClick={() => handleTagClick(keyword)}
               >
                 <TagLabel>{keyword}</TagLabel>
                 <TagCloseButton
                   onClick={(e) => {
-                    e.stopPropagation(); // 태그 클릭 이벤트 방지
+                    e.stopPropagation();
                     setKeywords(keywords.filter((_, i) => i !== index));
                   }}
                 />
@@ -240,30 +239,27 @@ function PutClothes() {
         <Flex
           mb="24px"
           flexDirection={{ base: 'column', sm: 'row' }}
-          alignItems="flex-start"
+          alignItems="center"
           width="100%"
         >
           <Text
             fontFamily="SUIT"
             fontSize="20px"
-            fontStyle="normal"
             fontWeight="700"
-            lineHeight="normal"
             color="var(--21-purple-dark, #411461)"
-            mr={{ base: '0', sm: '53px' }}
             mb={{ base: '10px', sm: '0' }}
+            mr={{ base: '0', sm: '30px' }}
             width={{ base: '100%', sm: 'auto' }}
           >
             카테고리
           </Text>
           <Select
-            width={{ base: '100%', sm: '229px' }}
-            ml={{ base: '0', sm: 'auto' }}
             placeholder="카테고리를 선택하세요"
             borderRadius="25px"
             fontSize="14px"
             height="32px"
-            flexShrink={0}
+            width={{ base: '100%', sm: 'auto' }}
+            flex="1"
             bg="#FFFFFF"
             boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
             backdropFilter="blur(25px)"
@@ -279,33 +275,30 @@ function PutClothes() {
         <Flex
           mb="24px"
           flexDirection={{ base: 'column', sm: 'row' }}
-          alignItems="flex-start"
+          alignItems="center"
           width="100%"
         >
           <Text
             fontFamily="SUIT"
             fontSize="20px"
-            fontStyle="normal"
             fontWeight="700"
-            lineHeight="normal"
             color="var(--21-purple-dark, #411461)"
-            mr={{ base: '0', sm: '53px' }}
             mb={{ base: '10px', sm: '0' }}
+            mr={{ base: '0', sm: '30px' }}
             width={{ base: '100%', sm: 'auto' }}
           >
             옷 사이즈
           </Text>
           <Select
             placeholder="사이즈를 선택하세요"
-            width={{ base: '100%', sm: '229px' }}
             borderRadius="25px"
             fontSize="14px"
             height="32px"
-            flexShrink={0}
+            width={{ base: '100%', sm: 'auto' }}
+            flex="1"
             bg="#FFFFFF"
             boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
             backdropFilter="blur(25px)"
-            ml={{ base: '0', sm: 'auto' }}
           >
             <option value="small">S</option>
             <option value="medium">M</option>
@@ -321,114 +314,101 @@ function PutClothes() {
           alignItems="flex-start"
           width="100%"
         >
+          {/* 라벨 */}
           <Text
             fontFamily="SUIT"
             fontSize="20px"
-            fontStyle="normal"
             fontWeight="700"
-            lineHeight="normal"
             color="var(--21-purple-dark, #411461)"
-            mr={{ base: '0', sm: '53px' }}
             mb={{ base: '10px', sm: '0' }}
+            mr={{ base: '0', sm: '30px' }} // 'sm' 이상에서 30px 간격
             width={{ base: '100%', sm: 'auto' }}
           >
             사이즈 리뷰
           </Text>
 
+          {/* 입력 필드 컨테이너 */}
           <Flex
-            ml="auto"
-            alignItems="center"
-            borderRadius="25px"
-            bg="#FFF"
-            boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
-            backdropFilter="blur(25px)"
+            direction="column"
             width={{ base: '100%', sm: 'auto' }}
+            flex="1"
           >
-            <Text
-              fontFamily="SUIT"
-              fontSize="14px"
-              fontWeight="700"
-              color="#411461"
-              borderRadius="5px"
-              ml="16px"
-              mr="5px"
-              textAlign="center"
-            >
-              키:
-            </Text>
-            <Input
-              ml="auto"
-              width={{ base: '70%', sm: '229px' }}
-              type="number"
-              step="1"
-              min="0"
-              mr={{ base: '0', sm: 'auto' }}
-              placeholder="키를 입력하세요 (단위: cm)"
-              value={height} // 키 상태 변수 연결
-              onChange={(e) => setHeight(e.target.value)}
+            {/* '키' 입력 필드 */}
+            <Flex
+              alignItems="center"
               borderRadius="25px"
               bg="#FFF"
-              fontSize="14px"
-              height="32px"
-              border="none"
-              flexShrink={0}
-              _focus={{
-                outline: 'none',
-                boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.15)',
-              }}
-            />
-          </Flex>
-        </Flex>
+              mb="10px"
+              boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
+              backdropFilter="blur(25px)"
+              width="100%"
+            >
+              <Text
+                fontFamily="SUIT"
+                fontSize="14px"
+                fontWeight="700"
+                color="#411461"
+                ml="16px"
+                mr="5px"
+                width="70px" // 라벨 너비를 동일하게 설정
+              >
+                키:
+              </Text>
+              <Input
+                type="number"
+                placeholder="키를 입력하세요 (단위: cm)"
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
+                borderRadius="25px"
+                bg="#FFF"
+                fontSize="14px"
+                height="32px"
+                border="none"
+                flex="1"
+                _focus={{
+                  outline: 'none',
+                  boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.15)',
+                }}
+              />
+            </Flex>
 
-        {/* 몸무게 */}
-        <Flex
-          mb="24px"
-          flexDirection={{ base: 'column', sm: 'row' }}
-          alignItems="flex-start"
-          width="100%"
-          ml="auto"
-        >
-          <Flex
-            alignItems="center"
-            ml={{ base: '0', sm: 'auto' }}
-            borderRadius="25px"
-            bg="#FFF"
-            boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
-            backdropFilter="blur(25px)"
-            width={{ base: '100%', sm: 'auto' }}
-          >
-            <Text
-              fontFamily="SUIT"
-              fontSize="14px"
-              fontWeight="700"
-              color="#411461"
-              borderRadius="5px"
-              ml="16px"
-              mr="5px"
-              textAlign="center"
-            >
-              몸무게:
-            </Text>
-            <Input
-              ml="auto"
-              width={{ base: '70%', sm: '229px' }}
-              type="number"
-              step="1"
-              min="0"
-              placeholder="몸무게를 입력하세요 (단위: kg)"
-              value={weight} // 몸무게 상태 변수 연결
-              onChange={(e) => setWeight(e.target.value)}
+            {/* '몸무게' 입력 필드 */}
+            <Flex
+              alignItems="center"
               borderRadius="25px"
               bg="#FFF"
-              fontSize="14px"
-              height="32px"
-              border="none"
-              flexShrink={0}
-              _focus={{
-                outline: 'none',
-                boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.15)',
-              }}
-            />
+              boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
+              backdropFilter="blur(25px)"
+              width="100%"
+            >
+              <Text
+                fontFamily="SUIT"
+                fontSize="14px"
+                fontWeight="700"
+                color="#411461"
+                ml="16px"
+                mr="5px"
+                width="70px" // 동일한 라벨 너비
+              >
+                몸무게:
+              </Text>
+              <Input
+                type="number"
+                placeholder="몸무게를 입력하세요 (단위: kg)"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+                borderRadius="25px"
+                bg="#FFF"
+                fontSize="14px"
+                height="32px"
+                border="none"
+                flex="1"
+                _focus={{
+                  outline: 'none',
+                  boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.15)',
+                }}
+              />
+            </Flex>
           </Flex>
         </Flex>
 
@@ -438,17 +418,10 @@ function PutClothes() {
           textAlign="right"
           fontFamily="SUIT"
           fontSize="12px"
-          fontStyle="normal"
           fontWeight="700"
-          lineHeight="normal"
-          textDecorationLine="underline"
-          textDecorationStyle="solid"
-          textDecorationSkipInk="none"
-          textDecorationThickness="auto"
-          textUnderlineOffset="auto"
-          textUnderlinePosition="from-font"
-          cursor="pointer" // 클릭 가능 표시
-          onClick={handleProfileSizeClick} // 클릭 시 더미 데이터 추가
+          textDecoration="underline"
+          cursor="pointer"
+          onClick={handleProfileSizeClick}
         >
           프로필 사이즈 가져오기
         </Text>
@@ -457,33 +430,30 @@ function PutClothes() {
         <Flex
           mb="24px"
           flexDirection={{ base: 'column', sm: 'row' }}
-          alignItems="flex-start"
+          alignItems="center"
           width="100%"
         >
           <Text
             fontFamily="SUIT"
             fontSize="20px"
-            fontStyle="normal"
             fontWeight="700"
-            lineHeight="normal"
             color="var(--21-purple-dark, #411461)"
-            mr={{ base: '0', sm: '53px' }}
             mb={{ base: '10px', sm: '0' }}
+            mr={{ base: '0', sm: '30px' }}
             width={{ base: '100%', sm: 'auto' }}
           >
             체감 사이즈
           </Text>
           <Select
-            placeholder="사이즈를 선택하세요"
-            width={{ base: '100%', sm: '229px' }}
+            placeholder="체감 사이즈를 선택하세요"
             borderRadius="25px"
             fontSize="14px"
             height="32px"
-            flexShrink={0}
+            width={{ base: '100%', sm: 'auto' }}
+            flex="1"
             bg="#FFFFFF"
             boxShadow="0px 2px 4px 1px rgba(0, 0, 0, 0.10)"
             backdropFilter="blur(25px)"
-            ml={{ base: '0', sm: 'auto' }}
           >
             <option value="small">작음</option>
             <option value="fit">딱 맞음</option>
